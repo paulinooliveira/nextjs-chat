c035f3784f0da748cb6612aefccfe2f9a0d3f369
Paulino <p.oliveira@belo.re>
2024-09-15 15:58:20 -0300

    Cleanup: Remove unused files and placeholders related to R execution

--- pages/api/hello.ts
+++
@@ -1,6 +0,0 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
-
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  console.log('Hello API route hit!');
-  res.status(200).json({ message: 'Hello from API' })
-}
